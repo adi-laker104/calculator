3602e1c841f0e65e064f4662632bd3c3906eaee3
Adi-laker104 <6759685+adi-laker104@users.noreply.github.com>
2024-08-31 15:02:53 -0700

    Add logic for remaining input buttons: both change sign logic and numberpad inputs logic

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+//Output value holder
+const outputDiv = document.getElementById("outputDiv");
+
 //AC Button Logic
 
 const clearButton = document.getElementById("clear");
-const output = document.getElementById("outputDiv");
 
-clearButton.addEventListener("click", function() {
-    const outputDiv = document.getElementById("outputDiv");
+clearButton.addEventListener("click", function() {   
     outputDiv.textContent = "";
 });
 
+// +/- Button Logic
+
+const changeSignButton = document.getElementById("change_sign");
+let i = 0;
+changeSignButton.addEventListener("click", function() {
+    if (i % 2 == 0) {
+    outputDiv.textContent = "-" + outputDiv.textContent;
+    } 
+    else {
+        outputDiv.textContent = 
+        outputDiv.textContent.substring(1,outputDiv.textContent.length);
+    }
+    i++;
+});
+
+
+//Input Button Logic
+const inputValues = document.querySelectorAll(".input");
+
+inputValues.forEach(element => {
+    element.addEventListener("click", function() {   
+        outputDiv.textContent += element.textContent;
+    })
+});
