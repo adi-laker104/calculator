41c423d9485b3f69e60d8d8d0daf48785b83b370
Adi-laker104 <6759685+adi-laker104@users.noreply.github.com>
2024-08-31 20:00:13 -0700

    created variables for operational elements

--- script.js
+++ script.js
@@ -27,9 +27,64 @@ changeSignButton.addEventListener("click", function() {
 
 //Input Button Logic
 const inputValues = document.querySelectorAll(".input");
+const dotValues = document.querySelectorAll(".dot");
+let dotIndex = 0;
 
 inputValues.forEach(element => {
     element.addEventListener("click", function() {   
         outputDiv.textContent += element.textContent;
     })
+});
+
+dotValues.forEach(element => {
+    element.addEventListener("click", function() {   
+        if (dotIndex == 0 ) {
+        //console.log(dotValues);
+        outputDiv.textContent += element.textContent;
+        }
+        dotIndex++;
+    })
+});
+
+//Add, Subtract, Multiply, Divide Functions
+
+function add (a,b) {
+    return a + b;
+}
+
+function subtract (a,b) {
+    return a - b;
+}
+
+function multiply (a,b) {
+    return a * b;
+}
+
+function divide (a,b) {
+    return a / b;
+}
+
+//Variable creation for first and second input number
+let firstInput = 0;
+let clickCounter = 0;
+let operatorVal = "";
+const operatorValues = document.querySelectorAll(".operator");
+const equalButton = document.getElementById("equal_button");
+operatorValues.forEach(operation => {   
+    operation.addEventListener("click",function() {
+        if (clickCounter % 2 == 0) 
+            {
+            operatorVal = operation.id;
+            console.log(operatorVal);
+            firstInput = outputDiv.textContent;
+            outputDiv.textContent = "";
+            }
+
+        else {
+            secondInput = outputDiv.textContent;
+            outputDiv.textContent = "";
+        }
+        clickCounter++;
+    })
+
 });
